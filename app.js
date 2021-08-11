a2a1f2284e36769332874fe638d14a82370a4c20
Justfemi <ajibadeolufemi94@gmail.com>
2021-08-11 13:36:08 +0100

    ft:added swiper to home

--- app.js
+++ app.js
@@ -4,4 +4,32 @@ let navbar = document.querySelector('.navbar');
 menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-})
+});
+
+window.addEventListener('scroll', () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+});
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+// --------------for swipper ------
+let swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
