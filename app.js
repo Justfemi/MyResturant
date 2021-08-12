f903feae9fc5221a39d4af656e8a5b8359e9f7cf
Justfemi <ajibadeolufemi94@gmail.com>
2021-08-12 08:13:30 +0100

    ft:project-copleted

--- app.js
+++ app.js
@@ -6,9 +6,28 @@ menu.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.navbar a');
+
 window.addEventListener('scroll', () => {
+     
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if( top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('.navbar a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
 });
 
 document.querySelector('#search-icon').onclick = () => {
@@ -20,7 +39,7 @@ document.querySelector('#close').onclick = () => {
 }
 
 // --------------for swipper ------
-let swiper = new Swiper(".home-slider", {
+let swiper1 = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
@@ -57,3 +76,13 @@ let swiper2 = new Swiper(".review-slider", {
         },
     },
 });
+
+// function loader() {
+//     document.querySelector('loader-container').classList.add('fade-out');
+// }
+
+// function fadeOut() {
+//     setInterval(loader, 3000);
+// }
+
+// window.onload = fadeOut();
